f41fbbf080a0a9691c7cb15e5b5f5f952dcb32f0
Dany.Gen <Gentiev@vk.com>
2024-02-20 11:54:41 +0300

    Baz lvl

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,107 @@
+import java.util.Objects;
 public class Employee {
+    private static int idCounter = 1;
+    private int id;
+    private String name;
+    private int department;
+    private double salary;
+
+    public Employee(String name, int department, double salary) {
+        this.id = idCounter++;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        Objects objects;
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee " + "id" + id + ", name: " + name + ", department: " + department + ", salary: " + salary + "\n";
+    }
+
+    //Summa vseh ZP
+    public static double findSumAllSalary(Employee[] employees) {
+        double total = 0.0;
+        for (Employee employee : employees) {
+            total += employee.salary;
+        }
+        return total;
+    }
+
+    //Min ZP
+    public static Employee findMinSalary(Employee[] employees) {
+        Employee minSalary = employees[0];
+        for (Employee employee : employees) {
+            if (employee.salary < minSalary.salary) {
+                minSalary = employee;
+            }
+        }
+        return minSalary;
+    }
+
+    //Max ZP
+    public static Employee findMaxSalary(Employee[] employees) {
+        Employee maxSalary = employees[0];
+        for (Employee employee : employees) {
+            if (employee.salary > maxSalary.salary) {
+                maxSalary = employee;
+            }
+        }
+        return maxSalary;
+    }
+
+    //Midl line of ZP
+    public static double findMidlSalary(Employee[] employees) {
+        double total = findSumAllSalary(employees);
+        return (double) total / employees.length;
+    }
+
+    //name all employees
+    public static void printAllName(Employee[] employees) {
+        System.out.println("\nСписок сотрудников:");
+        for (Employee employee : employees) {
+            System.out.println("   " + employee.name);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        Employee[] employees = new Employee[10];
+        employees[0] = new Employee("Andrey Ivanov", 1, 120000.0);
+        employees[1] = new Employee("Fedor Ivanov", 2, 112000.0);
+        employees[2] = new Employee("Alice Ivanova", 4, 105000.0);
+        employees[3] = new Employee("Albert Enstein", 3, 122000.0);
+        employees[4] = new Employee("Arcadiy Tsaregradcev", 2, 350000.0);
+        employees[5] = new Employee("Irina Generalskaya", 4, 126000.0);
+        employees[6] = new Employee("Andrey Kostenko", 1, 392000.0);
+        employees[7] = new Employee("Sergey Ekimov", 5, 108000.0);
+        employees[8] = new Employee("Samuel Pogosayn", 4, 60000.0);
+        employees[9] = new Employee("Andrey Silaev", 5, 120000.0);
+
+        for (Employee employee : employees)
+            if (employee != null) {
+                System.out.println(employee.toString());
+            }
+        System.out.println("Сумма всех затрат на зрплаты: \n" + Employee.findSumAllSalary(employees) + "\n");
+        System.out.println("Сотрудник с самой низкой зарплатой: \n" + Employee.findMinSalary(employees));
+        System.out.println("Сотрудник с самой высокой зарплатой: \n" + Employee.findMaxSalary(employees));
+        System.out.println("Средняя зарплата среди сотрудников: \n" + Employee.findMidlSalary(employees));
+        Employee.printAllName(employees);
     }
 }
